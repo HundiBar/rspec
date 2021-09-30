82e8ccd6d8591d7a9d46452ab66c0d2829ac7683
Dean Abbott <deanabbott293@gmail.com>
2021-09-30 11:14:58 +0900

    using let and lazy loading

--- spec/01-card_spec.rb
+++ spec/01-card_spec.rb
@@ -11,19 +11,20 @@ RSpec.describe Card do
   # end
 
   let(:card) { Card.new('Ace', 'Spades') }
+  #let(:x) { 1+1 } lazy loading -used when needed
+  #let(:y) { x + 10 }
 
   it 'has a rank and that rank can change' do
+    # y - if needed y then loads y then x then final y
     # expect(@card.rank).to eq('Ace')
     expect(card.rank).to eq('Ace')
     card.rank = 'Queen'
     expect(card.rank).to eq('Queen')
-
-
   end
 
   it 'has a suit' do
     # expect(@card.suit).to eq('Spades')
     expect(card.suit).to eq('Spades')
-
   end
+
 end
