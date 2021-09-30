34a99658da8a99b7e6d60c39f62fc8fc1f25ceea
Dean Abbott <deanabbott293@gmail.com>
2021-09-30 09:23:21 +0900

    modifying the card

--- spec/01-card_spec.rb
+++ spec/01-card_spec.rb
@@ -1,8 +1,9 @@
 require '01-card'
 
-RSpec.describe 'Card' do
+RSpec.describe Card do
   it 'has a type' do
-    card = Card.new('Ace of Spades')
-    expect(card.type).to eq('Ace of Spades')
+    card = Card.new('Ace', 'Spades')
+    expect(card.rank).to eq('Ace')
+    expect(card.suit).to eq('Spades')
   end
 end
