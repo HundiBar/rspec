ca16c19d41dcdbf865c6481d60664ab4ec8d2180
Dean Abbott <deanabbott293@gmail.com>
2021-10-07 10:25:44 +0900

    testing context scope

--- spec/02-context_spec.rb
+++ spec/02-context_spec.rb
@@ -1,13 +1,20 @@
 RSpec.describe '#even? method' do
   context 'with even number' do
+    subject { 5 }
     it 'should return true' do
       expect(4.even?).to eq(true)
+    end
+    it 'should equal 5' do
+      expect(subject).to eq(5)
     end
   end
 
   context 'with odd number' do
     it 'should return true' do
       expect(5.even?).to eq(false)
+    end
+    it 'should not be accessible in this context' do
+      expect(subject).to eq(5)
     end
   end
 end
