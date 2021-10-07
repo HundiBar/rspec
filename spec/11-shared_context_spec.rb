e6ac0fd688ff337eef370ba5b8823732f296c5b4
Dean Abbott <deanabbott293@gmail.com>
2021-10-07 09:40:05 +0900

    can be used acrosss multiple examples and reset each time

--- spec/11-shared_context_spec.rb
+++ spec/11-shared_context_spec.rb
@@ -1,3 +1,31 @@
 RSpec.shared_contex 'commmon' do
+  before do
+    @foods = []
+  end
+  def some_helper_method
+    5
+  end
+
+  let(:some_variable) { [1, 2, 3] }
+end
+
+RSpec.describe 'first example group' do
+  include_context 'commmon'
+
+  it 'can use outside instance variables' do
+    expect(@foods.length).to eq(0)
+    @foods << 'sushi'
+    expect(@foods.length).to eq(1)
+
+  end
+
+  it 'can reuse instance variables across diff examples' do
+    expect(@foods.length).to eq(0)
+  end
+
+  it 'can use shared helper methods' do
+    expect(some_helper_method).to eq(5)
+  end
 
 end
+
