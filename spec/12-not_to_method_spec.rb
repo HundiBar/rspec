96416be2c2fbd1eccfc09d32eb20a72a4dc7a2ec
Dean Abbott <deanabbott293@gmail.com>
2021-10-08 11:40:08 +0900

    not_to key method

--- spec/12-not_to_method_spec.rb
+++ spec/12-not_to_method_spec.rb
@@ -2,7 +2,7 @@ RSpec.describe 'not_to method' do
   it 'checks that two values do not match' do
     # expect(5).not_to eq(5) #failing
     # expect('Hello').not_to eq('hello') #passing
-    expect([1,2]).not_to eq([1,2,3])
+    expect([1,2]).not_to eq([1,2,3]) #passing
 
 
   end
