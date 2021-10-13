199dc6ca88d51303fd8bdaec6c3dbff59a887894
Dean Abbott <deanabbott293@gmail.com>
2021-10-13 11:04:08 +0900

    all matcher examples

--- spec/16-all_matcher_spec.rb
+++ spec/16-all_matcher_spec.rb
@@ -3,6 +3,14 @@ RSpec.describe 'all matcher' do
     # [5, 7, 9].each do |val|
     #   expect(val).to be_odd
     # end
-    expect([5,7,9].to all(be_odd))
+    expect([5,7,9]).to all(be_odd)
+    expect([4,6,8,10]).to all(be_even)
+    expect([[],[],[]]).to all(be_empty)
+    expect([0, 0]).to all(be_zero)
+    expect([5, 7, 9]).to all(be < 10)
+
+
+
+
   end
 end
