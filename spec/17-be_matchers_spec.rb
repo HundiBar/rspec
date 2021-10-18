a6a055c93093fd93e5c9eaecd82ff2fea06b2bc5
Dean Abbott <deanabbott293@gmail.com>
2021-10-18 11:11:55 +0900

    be matchers truthy

--- spec/17-be_matchers_spec.rb
+++ spec/17-be_matchers_spec.rb
@@ -1,5 +1,7 @@
 RSpec.describe 'be matchers' do
   it 'can test for truthiness' do
     expect(true).to  be_truthy
+    expect('Hello').to  be_truthy
+    #etc all values truthy except false or nil
   end
 end
