dba80f7d9edf4db8510e3b96318912a249e10d65
Dean Abbott <deanabbott293@gmail.com>
2021-10-18 11:12:47 +0900

    be matchers faslsy"

--- spec/17-be_matchers_spec.rb
+++ spec/17-be_matchers_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe 'be matchers' do
     expect('Hello').to  be_truthy
     #etc all values truthy except false or nil
   end
+
+  it 'can test for falsiness' do
+    expect(false).to be_falsy
+    expect(nil).to  be_falsy
+  end
 end
