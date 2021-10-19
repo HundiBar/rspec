0ea681ea8ada768957af999edf7458150d4d8ec2
Dean Abbott <deanabbott293@gmail.com>
2021-10-19 10:00:37 +0900

    change matcher negative arguments

--- spec/18-change_matcher_spec.rb
+++ spec/18-change_matcher_spec.rb
@@ -6,6 +6,9 @@ RSpec.describe 'change matcher' do
     # drawbacks - spec breaks on original example of subject\
     expect { subject.push(4) }.to change { subject.length }.by(1)
     #keeps test more adapatable, as increases by 1, not from x to y
+  end
 
+  it 'accepts negative arguemnts' do
+    expect {  subject.pop }
   end
 end
