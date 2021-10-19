dde69c5767d92a366ecffa65d030861f5b76415f
Dean Abbott <deanabbott293@gmail.com>
2021-10-19 10:00:05 +0900

    change matcher dynamic by argument

--- spec/18-change_matcher_spec.rb
+++ spec/18-change_matcher_spec.rb
@@ -2,6 +2,10 @@ RSpec.describe 'change matcher' do
   subject { [1,2,3] }
 
   it 'checks that a method changes object state' do
-    expect { subject.push(4) }.to change { subject.length }.from(3).to(4)
+    # expect { subject.push(4) }.to change { subject.length }.from(3).to(4)
+    # drawbacks - spec breaks on original example of subject\
+    expect { subject.push(4) }.to change { subject.length }.by(1)
+    #keeps test more adapatable, as increases by 1, not from x to y
+
   end
 end
