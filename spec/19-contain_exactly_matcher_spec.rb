b5c84c78c69d67cea687c0652dc58d5a4f536d60
Dean Abbott <deanabbott293@gmail.com>
2021-10-20 11:05:55 +0900

    checking different failure cases

--- spec/19-contain_exactly_matcher_spec.rb
+++ spec/19-contain_exactly_matcher_spec.rb
@@ -6,6 +6,12 @@ RSpec.describe 'contain_exactly matcher' do
       expect(subject).to contain_exactly(3, 2, 1)
       expect(subject).to contain_exactly(2, 1, 3)
 
+      # expect(subject).to contain_exactly(1, 2)
+      # failure as missing will show in rspec test
+      # expect(subject).to contain_exactly(3, 2, 1, 4)
+      # additional element fails in comparison
+
+
     end
   end
 end
