097b182cae232257108b78d3418dd293877d417b
Dean Abbott <deanabbott293@gmail.com>
2021-10-21 11:26:24 +0900

    start with and end with matchers

--- spec/20-start_with_and_end_with_matchers_spec.rb
+++ spec/20-start_with_and_end_with_matchers_spec.rb
@@ -1,23 +1,24 @@
-RSpec.describe 'start with and end with matcher' do
+RSpec.describe 'start_with and end_with matchers' do
   describe 'caterpillar' do
-    it 'should check for a substring at the beginning or end' do
+    it 'should check for substring at the beginning or end' do
       expect(subject).to start_with('cat')
       expect(subject).to end_with('pillar')
-      #careful with case sensitivity
     end
+
     it { is_expected.to start_with('cat') }
     it { is_expected.to end_with('pillar') }
   end
 
-  describe[:a, :b, :c, :d] do
+  describe [:a, :b, :c, :d] do
     it 'should check for elements at beginning or end of the array' do
       expect(subject).to start_with(:a)
       expect(subject).to start_with(:a, :b)
       expect(subject).to start_with(:a, :b, :c)
-      # if go further and have whole array check then can check for contains in if dont care about order
-      # or use equality matcher to check exact order
       expect(subject).to end_with(:d)
-
+      expect(subject).to end_with(:c, :d)
     end
+
+    it { is_expected.to start_with(:a, :b) }
   end
+
 end
