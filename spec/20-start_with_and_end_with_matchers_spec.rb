567ff31d190ad3dfd16d1c8cbef31c4d639be04f
Dean Abbott <deanabbott293@gmail.com>
2021-10-21 11:15:59 +0900

    start with and end with matchers

--- spec/20-start_with_and_end_with_matchers_spec.rb
+++ spec/20-start_with_and_end_with_matchers_spec.rb
@@ -3,7 +3,11 @@ RSpec.describe 'start with and end with matcher' do
     it 'should check for a substring at the beginning or end' do
       expect(subject).to start_with('cat')
       expect(subject).to end_with('pillar')
+      #careful with case sensitivity
     end
+    it { is_expected.to start_with('cat') }
+    it { is_expected.to end_with('pillar') }
+
   end
 
 end
