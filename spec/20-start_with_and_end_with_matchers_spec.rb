e3285b3a16bb7a848076f1d2a53a0152acd32821
Dean Abbott <deanabbott293@gmail.com>
2021-10-21 11:18:10 +0900

    start with and end with matchers

--- spec/20-start_with_and_end_with_matchers_spec.rb
+++ spec/20-start_with_and_end_with_matchers_spec.rb
@@ -7,7 +7,16 @@ RSpec.describe 'start with and end with matcher' do
     end
     it { is_expected.to start_with('cat') }
     it { is_expected.to end_with('pillar') }
-
   end
 
+  describe[:a, :b, :c, :d] do
+    it 'should check for elements at beginning or end of the array' do
+      expect(subject).to start_with(:a)
+      expect(subject).to start_with(:a, :b)
+      expect(subject).to start_with(:a, :b, :c)
+      # if go further and have whole array check then can check for contains in if dont care about order
+      # or use equality matcher to check exact order
+
+    end
+  end
 end
