8793e33ac38043e3fa975d60132ab3a2f4d211c2
Dean Abbott <deanabbott293@gmail.com>
2021-10-23 11:17:40 +0900

    have attributes matcher

--- spec/21-have_attributes_matcher_spec.rb
+++ spec/21-have_attributes_matcher_spec.rb
@@ -6,6 +6,12 @@ class ProfessionalWrestler
   end
 end
 RSpec.describe 'have attributes matcher' do
-  describe ProfessionalWrestler.new('Stone Cold', 'Stunner')
+  describe ProfessionalWrestler.new('Stone Cold', 'Stunner') do
+    it 'checks for object attribute and proper values' do
+      expect(subject).to have_attributes(name: 'Stone Cold')
+      expect(subject).to have_attributes(name: 'Stone Cold', finishing_move: 'Stunner')
+
+    end
+  end
 
 end
