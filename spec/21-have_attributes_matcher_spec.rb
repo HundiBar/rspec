1c907882f42998e546bd0cf345aa61c7415f0b38
Dean Abbott <deanabbott293@gmail.com>
2021-10-23 11:16:15 +0900

    have attributes matcher

--- spec/21-have_attributes_matcher_spec.rb
+++ spec/21-have_attributes_matcher_spec.rb
@@ -1,3 +1,11 @@
-RSpec.describe '' do
+class ProfessionalWrestler
+  attr_reader :name, :finishing_move
+  def initialize(name, finishing_move)
+    @name = name
+    @finishing_move = finishing_move
+  end
+end
+RSpec.describe 'have attributes matcher' do
+  describe ProfessionalWrestler.new('Stone Cold', 'Stunner')
 
 end
