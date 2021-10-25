8b5842414119c7732e59ea04bcf9967c6c078de5
Dean Abbott <deanabbott293@gmail.com>
2021-10-25 10:54:19 +0900

    inlcude matcher

--- spec/22-include_matcher_spec.rb
+++ spec/22-include_matcher_spec.rb
@@ -2,6 +2,9 @@ RSpec.describe 'include matcher' do
   describe 'hot choc' do
     it 'checks for substring inclusion' do
       expect(subject).to include('hot')
+      expect(subject).to include('choc')
+      expect(subject).to include('late')
+
     end
   end
 end
