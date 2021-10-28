bbd1bdc3b7e68b6fbc27b2cdb36000c76aae985b
Dean Abbott <deanabbott293@gmail.com>
2021-10-28 19:16:54 +0900

    respond to matcher

--- spec/24-respond_to_matcher_spec.rb
+++ spec/24-respond_to_matcher_spec.rb
@@ -1,3 +1,15 @@
 RSpec.describe '' do
+  class HotChocolate
+    def drink
+      'Delicious'
+    end
 
+    def discard
+      'PLOP!'
+    end
+
+    def purchase(number)
+      "Awesome, I just purchased #{number} more hot chocolate beverages"
+    end
+  end
 end
