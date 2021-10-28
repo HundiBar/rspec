f0e9a70ae3c0b2fe5935adec822f05647b68ae1e
Dean Abbott <deanabbott293@gmail.com>
2021-10-28 19:21:58 +0900

    respond to matcher

--- spec/24-respond_to_matcher_spec.rb
+++ spec/24-respond_to_matcher_spec.rb
@@ -1,21 +1,19 @@
-RSpec.describe '' do
-  class HotChocolate
-    def drink
-      'Delicious'
-    end
-
-    def discard
-      'PLOP!'
-    end
-
-    def purchase(number)
-      "Awesome, I just purchased #{number} more hot chocolate beverages"
-    end
+class HotChocolate
+  def drink
+    'Delicious'
   end
 
-  class Coffee
-    def drink; end
-    def discard; end
-    def purchase(number); end
+  def discard
+    'PLOP!'
+  end
+
+  def purchase(number)
+    "Awesome, I just purchased #{number} more hot chocolate beverages"
   end
 end
+
+# class Coffee
+#   def drink; end
+#   def discard; end
+#   def purchase(number); end
+# end
