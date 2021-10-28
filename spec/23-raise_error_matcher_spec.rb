2d4b5e88c47588eb428bdf3b7f5af74bbdaeb302
Dean Abbott <deanabbott293@gmail.com>
2021-10-28 18:56:17 +0900

    raise error matcher

--- spec/23-raise_error_matcher_spec.rb
+++ spec/23-raise_error_matcher_spec.rb
@@ -2,6 +2,8 @@ RSpec.describe 'raise_error matcher' do
   def some_method
     x
   end
+
+  class CustomError < StandardError; end
   it 'can check for a specific error beinr raised' do
     expect { some_method }.to raise_error(NameError)
     expect { 10/0 }.to raise_error(ZeroDivisionError)
