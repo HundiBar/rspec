236f1b81e7c215894dd68e47fb53177aac24b762
Dean Abbott <deanabbott293@gmail.com>
2021-10-29 13:45:58 +0900

    not-to  matcher

--- spec/26-not_to_spec.rb
+++ spec/26-not_to_spec.rb
@@ -5,7 +5,12 @@ RSpec.describe 'the not_to method' do
     expect([1,2,3]).not_to equal([1,2,3])
     expect(10).not_to be_odd
     expect([1,2,3]).not_to be_empty
+
     expect(nil).not_to be_truthy
+
+    expect("Philadelphia").not_to start_with('car')
+    expect("Philadelphia").not_to end_with('city')
+
 
   end
 end
