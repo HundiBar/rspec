ecd12a3fb67e7ee7bce873520bbab0d5b00b59fa
Dean Abbott <deanabbott293@gmail.com>
2021-10-29 13:22:22 +0900

    satisfy  matcher

--- spec/25-satisfy_matcher_spec.rb
+++ spec/25-satisfy_matcher_spec.rb
@@ -1,7 +1,16 @@
 RSpec.describe 'satisfy matcher' do
   subject { 'racecar' }
+  # subject { 'racecars' }
+
 
   it 'is a palindrome' do
     expect(subject).to satisfy { |value| value == value.reverse }
+  end
+
+  it 'can accept a custom error message' do
+    expect(subject).to satisfy('be a palindrome') do |value|
+      value == value.reverse
+      #can use { |value| value == value.reverse }
+    end
   end
 end
