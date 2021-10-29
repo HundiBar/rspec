27810758635e829250d92469ce59478bda40f59e
Dean Abbott <deanabbott293@gmail.com>
2021-10-29 13:43:29 +0900

    not-to  matcher

--- spec/26-not_to_spec.rb
+++ spec/26-not_to_spec.rb
@@ -1,5 +1,9 @@
 RSpec.describe 'the not_to method' do
   it 'checks for the inverse of a matcher' do
     expect(5).not_to eq(10)
+    #checks for same array in memory (equal)
+    expect([1,2,3]).not_to equal([1,2,3])
+
+
   end
 end
