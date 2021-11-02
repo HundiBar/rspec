c46f5ca6eef087c95873f0684b5c4c82316c9907
Dean Abbott <deanabbott293@gmail.com>
2021-11-02 12:37:44 +0900

    double spec assignment

--- spec/double_assignment_spec.rb
+++ spec/double_assignment_spec.rb
@@ -1,5 +1,9 @@
 RSpec.describe 'doubles' do
   it "tests the student's knowledge of the course's content" do
-    double("Database Connection")
+    db = double("Database Connection")
+    allow(db).to receive_messages(connect: true, disconnect: false,)
+    expect(db.connect).to eq(true)
+    expect(db.disconnect).to eq(false)
+
   end
 end
