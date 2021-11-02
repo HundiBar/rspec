e4b7f2835c0c043735780cd41ac69029e7bee89b
Dean Abbott <deanabbott293@gmail.com>
2021-11-02 12:40:56 +0900

    double spec assignment

--- spec/30-double_assignment_spec.rb
+++ spec/30-double_assignment_spec.rb
@@ -1,7 +1,14 @@
 RSpec.describe 'doubles' do
-  it "tests the student's knowledge of the course's content" do
+  it "check database connection double" do
     db = double("Database Connection")
     allow(db).to receive_messages(connect: true, disconnect: false,)
+    expect(db.connect).to eq(true)
+    expect(db.disconnect).to eq(false)
+
+  end
+  it "check database connection double" do
+    fs = double("File System")
+    allow(db).to receive_messages(read: "Romeo and Juliet", write: false,)
     expect(db.connect).to eq(true)
     expect(db.disconnect).to eq(false)
 
