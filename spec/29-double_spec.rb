ac97b3547ac8c552e0c9526d0b99df3319d28075
Dean Abbott <deanabbott293@gmail.com>
2021-11-02 12:24:23 +0900

    double spec

--- spec/29-double_spec.rb
+++ spec/29-double_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe 'a random double' do
   it 'only allows defined methods to be invoked' do
-    stuntman = double("Mr. Danger")
+    stuntman = double("Mr. Danger", fall_off_ladder: 'Ouch')
 
     stuntman.fall_off_ladder
   end
