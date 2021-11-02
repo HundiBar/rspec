0d51832f4263299a1cc420f9ab8681bc0afd7134
Dean Abbott <deanabbott293@gmail.com>
2021-11-02 12:29:39 +0900

    double spec

--- spec/29-double_spec.rb
+++ spec/29-double_spec.rb
@@ -6,5 +6,7 @@ RSpec.describe 'a random double' do
     stuntman = double("Mr.Danger")
     allow(stuntman).to receive(:fall_off_ladder).and_return('Ouch')
     allow(stuntman).to receive(:light_on_fire).and_return(true)
+    expect(stuntman.fall_off_ladder).to eq('Ouch')
+    expect(stuntman.light_on_fire).to eq(true)
   end
 end
