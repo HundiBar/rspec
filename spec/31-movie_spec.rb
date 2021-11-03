80b31d50a1d160df22acc2c0008587a73dc37e5c
Dean Abbott <deanabbott293@gmail.com>
2021-11-03 13:02:10 +0900

    double exmaple  classes

--- spec/31-movie_spec.rb
+++ spec/31-movie_spec.rb
@@ -58,7 +58,8 @@ RSpec.describe Movie do
       expect(stuntman).to receive(:light_on_fire).at_most(1).times
 
       # expect(stuntman).to receive(:act).twice
-      expect(stuntman).to receive(:act).exactly(2).times
+      # expect(stuntman).to receive(:act).exactly(2).times
+      expect(stuntman).to receive(:act).at_least(2).times
       subject.start_shooting
     end
   end
