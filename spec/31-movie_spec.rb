9719404837c17e762350f7cdcb3596e77ae12e7a
Dean Abbott <deanabbott293@gmail.com>
2021-11-03 12:57:55 +0900

    double exmaple  classes

--- spec/31-movie_spec.rb
+++ spec/31-movie_spec.rb
@@ -53,7 +53,8 @@ RSpec.describe Movie do
   describe 'movie shooting methods' do
     it 'expects an actor to do 3 actions' do
       # expect(stuntman).to receive(:light_on_fire).once
-      expect(stuntman).to receive(:light_on_fire).exactly(1).times
+      # expect(stuntman).to receive(:light_on_fire).exactly(1).times
+      expect(stuntman).to receive(:light_on_fire).at_most(1).times
       subject.start_shooting
     end
   end
