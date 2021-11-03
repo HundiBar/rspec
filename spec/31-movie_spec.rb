4f8da166154ee2ced145e0678e8898531d637921
Dean Abbott <deanabbott293@gmail.com>
2021-11-03 12:15:38 +0900

    double exmaple  classes

--- spec/31-movie_spec.rb
+++ spec/31-movie_spec.rb
@@ -19,5 +19,20 @@ class Actor
   def light_on_fire
     false
   end
+end
 
+class Movie
+  attr_reader :Actor
+
+  def initialize(actor)
+    @actor = actor
+  end
+
+  def start_shooting
+    if actor.ready?
+      actor.act
+      actor.fall_off_ladder
+      actor.light_on_fire
+    end
+  end
 end
