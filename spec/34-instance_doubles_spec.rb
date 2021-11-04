aeb56fe6a16c133150d949615c2701722dd78a63
Dean Abbott <deanabbott293@gmail.com>
2021-11-04 11:12:27 +0900

    instance doubles

--- spec/34-instance_doubles_spec.rb
+++ spec/34-instance_doubles_spec.rb
@@ -1,4 +1,12 @@
 # Rspec verifies that the object your mocking matches your double
+class Person
+  def a
+    #mock complex system takes 3 secs
+    sleep(3)
+    "Hello"
+  end
+
+end
 RSpec.describe 'instance doubles' do
 
 end
