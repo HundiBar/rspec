7a77272feceb92f84c389acc7897e45b64321ffa
Dean Abbott <deanabbott293@gmail.com>
2021-11-04 11:04:00 +0900

    matching arguments

--- spec/33-matching_arguments_spec.rb
+++ spec/33-matching_arguments_spec.rb
@@ -1,6 +1,11 @@
 RSpec.describe 'matching arguments' do
   it 'can return different values depending on the argument' do
+    #double is emulating array for our mocking
+    # with(actual value), then return the array with first(number) should mock to
     three_element_array = double #[1, 2, 3]
     allow(three_element_array).to receive(:first).with(no_args).and_return(1)
+    allow(three_element_array).to receive(:first).with(1).and_return([1])
+    allow(three_element_array).to receive(:first).with(2).and_return([1, 2])
+
   end
 end
