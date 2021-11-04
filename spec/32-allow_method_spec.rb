40f8fbfd35dcef9b52d5bec8efd5b296f5dac4b4
Dean Abbott <deanabbott293@gmail.com>
2021-11-04 10:45:07 +0900

    allow method

--- spec/32-allow_method_spec.rb
+++ spec/32-allow_method_spec.rb
@@ -2,7 +2,6 @@ RSpec.describe 'allow method review' do
   it 'can customize return value for methods on doubles' do
     calculator = double
     allow(calculator).to receive(:add).add_return(15)
-
     expect(calculator.add).to eq(15)
   end
 end
