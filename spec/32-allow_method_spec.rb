4bf3a0c7ca305416aca0bcb4b2d349c25d0fdd82
Dean Abbott <deanabbott293@gmail.com>
2021-11-04 10:43:52 +0900

    allow method

--- spec/32-allow_method_spec.rb
+++ spec/32-allow_method_spec.rb
@@ -1,3 +1,6 @@
-RSpec.describe '' do
-
+RSpec.describe 'allow method review' do
+  it 'can customize return value for methods on doubles' do
+    calculator = double
+    allow(calculator).to receive(:add)
+  end
 end
