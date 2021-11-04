bd51d6f57484f5f0cd5c06ca11ddb7107a1387f1
Dean Abbott <deanabbott293@gmail.com>
2021-11-04 11:16:53 +0900

    instance doubles

--- spec/34-instance_doubles_spec.rb
+++ spec/34-instance_doubles_spec.rb
@@ -10,9 +10,16 @@ end
 RSpec.describe Person do
   describe 'regular double' do
     it 'can implement any method' do
+      # added in extra method b on the double that isn't same as the actually Person. Need to be as close as possible.
       person = double(a: "Hello", b: 20)
       expect(person.a).to eq("Hello")
     end
-
+  end
+  # Need to more cloesly align double and real object with RSpec verify with instance doubles
+  describe 'instance double' do
+    it 'can only implement methods that are defined on the class' do
+      # making a mock of any instance of the Person class not mocking Person class like above.
+      person = instance_double(Person)
+    end
   end
 end
