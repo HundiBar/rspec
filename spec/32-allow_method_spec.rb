e3a3676d1ee03ec400e8c89117f6c8747ca4fb65
Dean Abbott <deanabbott293@gmail.com>
2021-11-04 10:49:07 +0900

    allow method

--- spec/32-allow_method_spec.rb
+++ spec/32-allow_method_spec.rb
@@ -7,6 +7,16 @@ RSpec.describe 'allow method review' do
 
   it 'can stub one or more methods on a real object' do
     arr = [1, 2, 3]
+    # when sum called always returns 10
+    allow(arr).to recieve(:sum).and_return(10)
+    # test fails for real sum as RSpec steps in and mocks the sum method
     expect(arr.sum).to eq(6)
+
+    #all other arr methods are working normally, just mocked sum
+    arr.push(4)
+    expect(arr).to eq([1, 2, 3, 4])
+
+    # use allow to mock a complicated method on an object to stop the lengthy/complex real method when tested.
+
   end
 end
