534cc30e46f4a4e26796186c7e83b0bf2e8e5648
Dean Abbott <deanabbott293@gmail.com>
2021-11-04 10:41:03 +0900

    double exmaple  classes

--- spec/31-movie_spec.rb
+++ spec/31-movie_spec.rb
@@ -1,26 +1,26 @@
-class Actor
-  def initialize(name)
-    @name = name
-  end
+# class Actor
+#   def initialize(name)
+#     @name = name
+#   end
 
-  def ready?
-    sleep(3)
-    true
-  end
+#   def ready?
+#     sleep(3)
+#     true
+#   end
 
-  def act
-    'I love you baby'
-  end
+#   def act
+#     'I love you baby'
+#   end
 
-  def fall_off_ladder
-    "Call my agent no way"
-  end
+#   def fall_off_ladder
+#     "Call my agent no way"
+#   end
 
-  def light_on_fire
-    false
-  end
-end
-
+#   def light_on_fire
+#     false
+#   end
+# end
+# DONT NEED ACTOR CLASS AS WE CAN USE DOUBLE TO REPLICATE
 class Movie
   attr_reader :actor
 
