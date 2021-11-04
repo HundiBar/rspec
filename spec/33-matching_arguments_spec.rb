727cf22481f663e2b361d9ac8a28d08a9aa0bc6c
Dean Abbott <deanabbott293@gmail.com>
2021-11-04 11:02:12 +0900

    matching arguments

--- spec/33-matching_arguments_spec.rb
+++ spec/33-matching_arguments_spec.rb
@@ -1,5 +1,6 @@
 RSpec.describe 'matching arguments' do
   it 'can return different values depending on the argument' do
-
+    three_element_array = double #[1, 2, 3]
+    allow(three_element_array).to receive(:first).with(no_args).and_return(1)
   end
 end
