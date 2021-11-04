56883fc4c1f28612bbb240435add0f1697af54e1
Dean Abbott <deanabbott293@gmail.com>
2021-11-04 11:05:31 +0900

    matching arguments

--- spec/33-matching_arguments_spec.rb
+++ spec/33-matching_arguments_spec.rb
@@ -6,6 +6,10 @@ RSpec.describe 'matching arguments' do
     allow(three_element_array).to receive(:first).with(no_args).and_return(1)
     allow(three_element_array).to receive(:first).with(1).and_return([1])
     allow(three_element_array).to receive(:first).with(2).and_return([1, 2])
+    # allow first to act on array but must be a value >=3 so first(3)
+    # using our double to mock behaviour
+    allow(three_element_array).to receive(:first).with(be >= 3).and_return([1, 2, 3])
+
 
   end
 end
