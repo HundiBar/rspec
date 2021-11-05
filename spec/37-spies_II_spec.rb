480499b0e5648e85199c3d3ffe0d3b5c0da26727
Dean Abbott <deanabbott293@gmail.com>
2021-11-05 11:22:22 +0900

    spies II

---
+++ spec/37-spies_II_spec.rb
@@ -0,0 +1,16 @@
+class Car
+  def initialize(model)
+    @model = model
+  end
+end
+
+class Garage
+  attr_reader :storage
+  def initialize
+    @storage = []
+  end
+
+  def add_to_collection(model)
+    @storage << Car.new(model)
+  end
+end
