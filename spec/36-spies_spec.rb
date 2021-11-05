694f6fe56d519c2d45f7dbd94204ec61cc5e5553
Dean Abbott <deanabbott293@gmail.com>
2021-11-05 11:12:18 +0900

    spies

--- spec/36-spies_spec.rb
+++ spec/36-spies_spec.rb
@@ -1,3 +1,8 @@
+# spies follow different pattern compard to doubles
+# doubles write expectation before the action been invoke
+#spy writes after action
+#Spies automatically observe all messages received even if not explicitly told
+
 RSpec.describe '' do
 
 end
