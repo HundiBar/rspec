fc9579550fcd27ad3f1d53072603ebd45a138abe
Dean Abbott <deanabbott293@gmail.com>
2021-11-05 10:51:34 +0900

    class double methods

--- spec/35-class_double_spec.rb
+++ spec/35-class_double_spec.rb
@@ -17,6 +17,8 @@ RSpec.describe CardGame do
     # using class double instead of double means double is as close as possible to real object as it compares to real object. Double will allow us to run non-existent methods in the class.
 
     #Will get an uninitialized constant Deck error as the constant is called Deck as an object so use a string to avoid that error.
-    class_double('Deck', build: ['Ace', 'Queen'])
+
+    #as_stubbed_const make sure that all calls to class method Deck with the class double in deck_klass
+    deck_klass = class_double('Deck', build: ['Ace', 'Queen']).as_stubbed_const
   end
 end
