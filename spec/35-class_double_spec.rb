99789234772c201baac30f08c27ade58bffe10e5
Dean Abbott <deanabbott293@gmail.com>
2021-11-05 10:50:09 +0900

    class double methods

--- spec/35-class_double_spec.rb
+++ spec/35-class_double_spec.rb
@@ -1,12 +1,12 @@
-attr_reader :cards
-class Deck
-  def self.build
-    #implement business logic to build bunch of cards
-  end
+# class Deck
+#   def self.build
+#     #implement business logic to build bunch of cards
+#   end
 
-end
+# end
 
 class CardGame
+  attr_reader :cards
   def start
     @cards = Deck.build
   end
@@ -15,6 +15,8 @@ end
 RSpec.describe CardGame do
   it 'can only implement class methods that are defined on a class' do
     # using class double instead of double means double is as close as possible to real object as it compares to real object. Double will allow us to run non-existent methods in the class.
+
+    #Will get an uninitialized constant Deck error as the constant is called Deck as an object so use a string to avoid that error.
     class_double('Deck', build: ['Ace', 'Queen'])
   end
 end
